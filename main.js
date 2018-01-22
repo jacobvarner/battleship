3af18f2ed1c21ae3d4e559eb9ede6f4e82228f2d
Jacob Varner <jacob.varner14@gmail.com>
2018-01-21 23:31:08 -0600

    Added functionallity for placing player ships. If I have time, I'd like to actually make certain steps work asynchronously instead of just hiding it with UI changes.

--- main.js
+++ main.js
@@ -1,28 +1,7 @@
 var playing = false;
 var playerFleet, opponentFleet, playerBoard, opponentBoard;
 
-/*
-*
-* Boards are represented by 2D arrays.
-* 0 = nothing
-* 1 = ship
-* 2 = miss
-* 3 = hit
-*
-*/
-
-var defaultBoard = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+var acceptedValues = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
 function Ship(name, size, hits=0) {
     this.name = name;
@@ -43,6 +22,11 @@ $(document).ready(function(){
             newGame();
         }
     });
+
+    $("#orientation-toggle").click(function () {
+        console.log("Orientation toggle clicked!");
+        $("#orientation-indicator").toggleClass("vertical");
+    });
 });
 
 function newGame() {
@@ -50,11 +34,12 @@ function newGame() {
     playing = true;
     playerFleet = createFleet();
     opponentFleet = createFleet();
-    playerBoard = defaultBoard;
-    opponentBoard = defaultBoard;
+    playerBoard = createBoard();
+    opponentBoard = createBoard();
     console.log("New game has been strated.");
 
     $("#output-text").text("Your opponent is placing their ships. Hang tight.");
+    
     placeOpponentShips();
 
     $("#output-text").text("Your opponent has placed their ships. It is now your turn to place your ships.");
@@ -65,14 +50,39 @@ function newGame() {
 }
 
 function createFleet() {
-    var carrier = new Ship("carrier", 5);
-    var battleship = new Ship("battleship", 4);
-    var cruiser = new Ship("cruiser", 3);
-    var submarine = new Ship("submarine", 3);
-    var destroyer = new Ship("destroyer", 2);
+    var carrier = new Ship("Carrier", 5);
+    var battleship = new Ship("Battleship", 4);
+    var cruiser = new Ship("Cruiser", 3);
+    var submarine = new Ship("Submarine", 3);
+    var destroyer = new Ship("Destroyer", 2);
 
-    var playerFleet = [carrier, battleship, cruiser, submarine, destroyer];
-    return playerFleet;
+    var fleet = [carrier, battleship, cruiser, submarine, destroyer];
+    return fleet;
+}
+
+function createBoard() {
+    /*
+    *
+    * Boards are represented by 2D arrays.
+    * 0 = nothing
+    * 1 = ship
+    * 2 = miss
+    * 3 = hit
+    *
+    */
+    var board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    return board;
 }
 
 function playBattleship() {
@@ -91,6 +101,8 @@ function placeOpponentShips() {
             y = Math.floor(Math.random() * 10);
         } while (placementCheck(opponentBoard, opponentFleet, ship, shipLength, orientation, x, y) === false);  
     }
+
+    return true;
 }
 
 function placementCheck(board, fleet, shipNumber, shipLength, orientation, x, y) {
@@ -119,12 +131,60 @@ function placementCheck(board, fleet, shipNumber, shipLength, orientation, x, y)
     for (pair in coordinates) {
         board[coordinates[pair][1]][coordinates[pair][0]] = 1;
     }
-    opponentFleet[ship].coordinates = coordinates;
+    fleet[ship].coordinates = coordinates;
     return true;
 }
 
 function placePlayerShips() {
-    
+    var ship = 0, orientation, x, y, shipLength;
+
+    $("#placement-button").click(function(){
+        shipLength = playerFleet[ship].size;
+        if ($("#orientation-indicator").hasClass("vertical")) {
+            orientation = 1;
+        } else {
+            orientation = 0;
+        }
+        var inputValue = $("#placement-location").val();
+        if (checkInputValue(inputValue) === false) {
+            alert("You must enter a valid Letter-Number pair with a letter before K and a positive number less than 10.\n\nEx. 'A3', 'C9', 'H5', etc.");
+        } else {
+            y = parseInt(inputValue[1]);
+            x = acceptedValues.indexOf(inputValue[0].toUpperCase());
+        }
+
+        if (placementCheck(playerBoard, playerFleet, ship, shipLength, orientation, x, y) === true) {
+            var pair;
+            var coordinates = playerFleet[ship].coordinates;
+            for (pair in coordinates) {
+                $("#p-" + acceptedValues[coordinates[pair][1]] + "" + coordinates[pair][0]).addClass("ship-space");
+            }
+            ship++;
+            if (ship > 4) {
+                $("#output-text").html("Great! All of your ships are in position and you are ready for battle.<br/><br/>You go first.");
+                $("#ship-placement-container").css("display", "none");
+                $("#firing-container").css("display", "block");
+            }
+            $("#ship-label > span").attr("id", "place-" + playerFleet[ship].name).text(playerFleet[ship].name);
+        } else {
+            alert("That ship doesn't fit there. Please enter another location where the ship will fit.");
+        }
+    });
+}
+
+function checkInputValue(value) {
+    if (value.length != 2) {
+        return false;
+    }
+    var test = parseInt(value[1]);
+    if (test == NaN) {
+       return false;
+    }
+    var test2 = value[0];
+    if (acceptedValues.indexOf(test2.toUpperCase()) == -1) {
+        return false;
+    }
+    return true;
 }
 
 function opponentTurn() {
